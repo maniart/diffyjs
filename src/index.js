ac8104b576a5490091bedbed1cf5e637924e9137
maniart <mani.art@gmail.com>
2016-12-22 14:46:03 -0500

    More work on diffy

--- src/index.js
+++ src/index.js
@@ -3,13 +3,44 @@
 import ReadableStream from 'readable-stream';
 import requestAnimFrame from './raf';
 import capture from './capture';
-import createOnceLog from './oncelog';
+import { createOnceLog, $ } from './utils';
 
 
 /*
   logger instances
 */
 const onceLog_1 = createOnceLog();
+
+/*
+
+*/
+const currentImageData = null;
+
+/*
+
+*/
+const previousImageData = null;
+
+/*
+
+*/
+const $debug = $('#diffy-debug');
+
+/*
+
+*/
+const $toggle = $('#diffy-toggle');
+
+/*
+
+*/
+const $video = $('#diffy-video');
+
+/*
+
+*/
+
+
 
 /*
   constraints object for getUserMedia
@@ -22,16 +53,40 @@ const constraints = {
   }
 };
 
-export default class Diffy {
-  constructor(props) {
-    this.foo = props;
-    this.stream = new ReadableStream();
-  }
+const VERSION = '1.0.0';
+const inctanceCount = 0;
 
-  createStream() {
-    console.log('create stream here');
-  }
+const create = () => {
+  console.log('Create diffy');
 }
+
+export default create;
+
+// const diffy = {
+//   VERSION: '1.0.0',
+//   instanceCount: 0,
+//   createDiffy() {
+//     if (instanceCount !== 0) {
+//       throw new Error('It seems like an instance of diffy has already been created in this page.');
+//     }
+//     this.instanceCount += 1;
+//     console.log('Yay! Diffy Making!');
+
+//   }
+// }
+
+// export default diffy;
+
+// export default class Diffy {
+//   constructor(props) {
+//     this.foo = props;
+//     this.stream = new ReadableStream();
+//   }
+
+//   createStream() {
+//     console.log('create stream here');
+//   }
+// }
 
 const loop = () => {
   onceLog_1('Loop is running.');
