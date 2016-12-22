43127df6f22972952fbc3c99a609e56fb0ecd3de
maniart <mani.art@gmail.com>
2016-12-21 23:48:36 -0500

    WIP webpack

--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@ export default class Diffy {
     console.log('create stream here');
   }
 }
-console.log('hi')
+console.log('hsssssssss')
 
 
--- tools/demo-server.js
+++ tools/demo-server.js
@@ -4,10 +4,10 @@ import express from 'express';
 import path from 'path';
 import webpack from 'webpack';
 import open from 'open';
-import config from '../webpack.config';
+import config from '../webpack.config.demo';
 
 const app = express();
-const port = 3000;
+const port = 4000;
 const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
@@ -18,7 +18,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../demo/index.html'));
 });
 
 app.listen(port, (err) => {
