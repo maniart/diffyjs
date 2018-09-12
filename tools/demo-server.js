e82f681239b69cee3dd296c8a5c11eae4ea215e1
maniart <mani.art@gmail.com>
2018-09-12 11:43:30 -0400

    Update demo server port to 5000

--- tools/demo-server.js
+++ tools/demo-server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = 4000;
+const port = 5000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../demo/index.html'));
