eabfd5351d0b55a4a7a3e2756f154f3e72058a47
maniart <mani.art@gmail.com>
2019-05-10 08:37:24 -0400

    Up the version number

--- src/Diffy.js
+++ src/Diffy.js
@@ -56,7 +56,7 @@ export default class Diffy {
 
     this.initialized = false;
 
-    this.VERSION = '1.3.3';
+    this.VERSION = '1.3.5';
 
     _win.addEventListener('load', this.init.bind(this));
   }
